d34de33810b3fb5171e7b00b363a69656b2790a3
MANYA SAJWAN <manya.21514@sscbs.du.ac.in>
2023-12-06 19:17:18 +0530

    changes done

--- q6/question6.js
+++ q6/question6.js
@@ -1,18 +1,22 @@
-function submission() {
-    var pname = document.getElementById('first').value;
-    var age = document.getElementById('second').value;
-    var weight = document.getElementById('third').value;
-    var type = document.getElementById('fourth').value;
-    var fav = document.getElementById('fourth').value;
+var pinfo = document.querySelector('.pet-info')
+var pname = document.querySelector('#first')
+var age = document.querySelector('#second')
+var weight = document.querySelector('#third')
+var type = document.querySelector('#fourth')
+var fav = document.querySelector('#fifth')
+const onSubmitHandler = e => {
+    e.preventDefault();
 
     var petObject = {
-        name: pname,
-        age: age,
-        weight: weight,
-        type: type,
-        fav: fav
+        name: pname.value,
+        age: age.value,
+        weight: weight.value,
+        type: type.value,
+        fav: fav.value
     };
     // Log Pet object and JSON equivalent to the console
     console.log('Pet Object:', petObject);
     console.log('JSON:', JSON.stringify(petObject));
-}
+};
+
+pinfo.addEventListener("submit", onSubmitHandler);
